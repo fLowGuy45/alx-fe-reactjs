975f1a0f8d90ea5c4aa19e2c0dd71d0dbdf2905f
Othniel <azureoniel19@gmail.com>
2025-11-09 20:01:02 +0000

    Refactored userData prop drilling to use Context API in alx-react-app-props

--- alx-react-app-props/src/UserContext.js
+++ alx-react-app-props/src/UserContext.js
@@ -1,45 +1,5 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import WelcomeMessage from './components/WelcomeMessage'
-import UserContext from './UserContext'
+import { createContext } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
-  const [user, setUser] = useState({
-    name: 'Alice',
-    age: 25,
-    bio: 'Loves hiking and photography.'
-  })
+const UserContext = createContext();
 
-  return (
-    <UserContext.Provider value={user}>
-      <>
-        <WelcomeMessage />
-        <div>
-          <a href="https://vite.dev" target="_blank">
-            <img src={viteLogo} className="logo" alt="Vite logo" />
-          </a>
-          <a href="https://react.dev" target="_blank">
-            <img src={reactLogo} className="logo react" alt="React logo" />
-          </a>
-        </div>
-        <h1>Vite + React</h1>
-        <div className="card">
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is {count}
-          </button>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test HMR
-          </p>
-        </div>
-        <p className="read-the-docs">
-          Click on the Vite and React logos to learn more
-        </p>
-      </>
-    </UserContext.Provider>
-  )
-}
-
-export default App
+export default UserContext;
