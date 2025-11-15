aefe15a5bbe81267f4a5bf3d342585714ae29850
Othniel <azureoniel19@gmail.com>
2025-11-15 17:18:18 +0000

    Fixed routing implementation by adding Router wrapper

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './pages/Home';
 import RecipeDetails from './pages/RecipeDetails';
 import EditRecipeForm from './components/EditRecipeForm';
